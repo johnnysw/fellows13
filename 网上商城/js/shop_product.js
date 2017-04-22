337966d57d3e3514a5b3de4449b6303d0ddaacca
dongxiuling <dxl_web@163.com>
2017-04-22 16:44:01 +0800

    事件

--- 网上商城/js/shop_product.js
+++ 网上商城/js/shop_product.js
@@ -1,3 +1,4 @@
+// 选项卡
 var oTabBox = document.getElementById('tab-box'),
 	aLi = oTabBox.getElementsByTagName('li'),
 	aDiv = getClass('tab-item',oTabBox);
@@ -13,8 +14,49 @@ var oTabBox = document.getElementById('tab-box'),
 			aDiv[this.index].style.display = 'block';
 		}
 	}
+// 下拉
+var oSelBox = document.getElementById('select-box');
+var oBtn = getClass('sel-btn',oSelBox)[0];
+var oUl = getClass('sel-menu',oSelBox)[0];
+var aOption = oUl.getElementsByTagName('li');
+var oP = oBtn.getElementsByTagName('p')[0];
+
+oBtn.onmousedown = function(){
+	oSelBox.className = 'select-box selected';
+}
+oBtn.onmouseup = function(){
+	oSelBox.className = 'select-box';
+
+	if(oUl.style.display == 'none' || oUl.style.display == ''){
+		oUl.style.display = 'block';
+	}else{
+		oUl.style.display = 'none';
+	}
+}
+
+for(var i=0; i<aOption.length;i++){
+	aOption[i].onclick = function(){
+		oP.innerHTML = this.innerHTML;
+		oUl.style.display = 'none';
+		// console.log(this.innerHTML);
+	}
+}
+
+document.onclick = function(e){
+	if(e.target != oSelBox){
+		oUl.style.display = 'none';
+	}
+
+}
 
 
-	// setInterval(function(){
-	// 	console.log("hahh");
-	// },3000)
+
+
+
+
+
+
+
+
+
+
