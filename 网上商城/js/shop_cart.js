099be3e6167d9601d37cb109065ffb3879ea50ef
dongxiuling <dxl_web@163.com>
2017-04-23 16:18:14 +0800

    购物车页面

--- 网上商城/js/shop_cart.js
+++ 网上商城/js/shop_cart.js
@@ -1,11 +1,11 @@
 var oTbody = document.getElementsByTagName('tbody')[0];
 var aTr = oTbody.getElementsByTagName('tr');
 var oCheckAll = document.getElementById('checkAll');
+var aCheckBtn = getClass('checkBtn',oTbody);
 for(var i=0; i<aTr.length; i++){
 	aTr[i].onclick = function(){
-
-
 		var oCheck = this.getElementsByTagName('input')[0];
+		//判断是否被选中
 		if(this.className == 'selected'){//已经被选中
 			this.className = '';
 			oCheck.checked = false;
@@ -13,7 +13,6 @@ for(var i=0; i<aTr.length; i++){
 			this.className = 'selected';
 			oCheck.checked = true;
 		}
-
 		//判断是否全被勾上
 		var aCheckTr = getClass('selected',oTbody);
 		if(aCheckTr.length == aTr.length){
@@ -23,4 +22,49 @@ for(var i=0; i<aTr.length; i++){
 		}
 	}
 }
+//全选按钮点击
+/*oCheckAll.onclick = function(){
+	console.log(this.checked);
+	for(var i=0; i<aTr.length; i++){
+		if(this.checked == true){
+			aTr[i].className = 'selected';
+		}else{
+			aTr[i].className = '';
+		}
+		aCheckBtn[i].checked = this.checked;
+	}
+}*/
+
+var oThead = document.getElementsByTagName('thead')[0];
+var oTr = oThead.getElementsByTagName('tr')[0];
+oTr.onclick = function(e){
+	var target = e.target || event.srcElement;
+	if(target != oCheckAll){
+		oCheckAll.checked = !oCheckAll.checked;
+	}
+    for(var i=0; i<aTr.length; i++){
+        if(oCheckAll.checked == true){
+            aTr[i].className = 'selected';
+        }else{
+            aTr[i].className = '';
+        }
+        aCheckBtn[i].checked = oCheckAll.checked;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- 网上商城/js/test.js
+++ 网上商城/js/test.js
@@ -1,53 +1,70 @@
-/**
- * Created by apple on 17/4/17.
- */
-
-function getClass(clsName,context){
-    var result = [];
-    /* if(context == undefined){
-     var arr = document.getElementsByTagName('*');
-     }else{
-     var arr = context.getElementsByTagName('*');
-     }*/
-    context = context || document;
-
-    var arr = context.getElementsByTagName('*');
-
-    for(var i=0; i<arr.length;i++){
-        //'aabb'   'aa'
-        if(arr[i].className.indexOf(clsName) != -1){
-            result.push(arr[i]);
+var oTbody = document.getElementsByTagName('tbody')[0];
+var aTr = oTbody.getElementsByTagName('tr');
+var oCheckAll = document.getElementById('checkAll');
+var aCheckBtn = getClass('checkBtn',oTbody);
+for(var i=0; i<aTr.length; i++){
+    aTr[i].onclick = function(){
+        var oCheck = this.getElementsByTagName('input')[0];
+        //判断是否被选中
+        if(this.className == 'selected'){//已经被选中
+            this.className = '';
+            oCheck.checked = false;
+        }else{
+            this.className = 'selected';
+            oCheck.checked = true;
+        }
+        //判断是否全被勾上
+        var aCheckTr = getClass('selected',oTbody);
+        if(aCheckTr.length == aTr.length){
+            oCheckAll.checked = true;
+        }else{
+            oCheckAll.checked = false;
         }
     }
-    return result;
+}
+//全选按钮点击
+/*oCheckAll.onclick = function(){
+    console.log(this.checked);
+    for(var i=0; i<aTr.length; i++){
+        if(this.checked == true){
+            aTr[i].className = 'selected';
+        }else{
+            aTr[i].className = '';
+        }
+        aCheckBtn[i].checked = this.checked;
+    }
+}*/
+
+var oThead = document.getElementsByTagName('thead')[0];
+var oTr = oThead.getElementsByTagName('tr')[0];
+oTr.onclick = function(){
+    oCheckAll.checked = true;
+    
+    for(var i=0; i<aTr.length; i++){
+        if(oCheckAll.checked == true){
+            aTr[i].className = 'selected';
+        }else{
+            aTr[i].className = '';
+        }
+        aCheckBtn[i].checked = oCheckAll.checked;
+    }
+
 }
 
-var oBox = document.getElementById('nav');
 
-var aLi = getClass('menu-btn',oBox);
 
-for(var i=0; i<aLi.length;i++){
-    aLi[i].onmouseover = function(){
 
-        //console.log(i);
 
-        //console.log(this);
-        var oUl = this.getElementsByTagName('ul')[0];
-        oUl.style.display = 'block';
 
-    }
-    aLi[i].onmouseout = function(){
 
-        //console.log(i);
 
-        //console.log(this);
-        var oUl = this.getElementsByTagName('ul')[0];
-        oUl.style.display = 'none';
 
-    }
-}
 
-console.log(i+"ashgdkhsgdfk");
+
+
+
+
+
 
 
 
