f0bcece116dfef531245825c34355cf319749d81
dongxiuling <dxl_web@163.com>
2017-05-17 19:53:21 +0800

    下落的树叶

--- requireJs/面向对象/index.js
+++ requireJs/面向对象/index.js
@@ -1,4 +1,5 @@
 var screenWidth = document.documentElement.clientWidth || document.body.clientWidth;
+var screenHeight= document.documentElement.clientHeight || document.body.clientHeight;
 function Leaf(){
 	/*属性：位置、速度、大小   方法：初始化、下落*/
 	this.width = 100+Math.random()*50;  //叶子宽度随机100-150
@@ -8,18 +9,40 @@ function Leaf(){
 		top:0
 	}
 	this.speed = 5;
+	this.img = new Image();
+	this.img.src="img/"+(Math.floor(Math.random()*4)+1)+".png";
+	this.img.style.width = this.width+"px";
+	this.img.style.left = this.pos.left +"px";
 }
 Leaf.prototype.init = function(){
-	var oImg = new Image();
-	oImg.src="img/"+(Math.floor(Math.random()*4)+1)+".png";
-	oImg.style.width = this.width+"px";
-	oImg.style.left = this.pos.left +"px";
-	document.body.appendChild(oImg);
+	document.body.appendChild(this.img);
 }
+Leaf.prototype.fall = function(){
+	var _this = this;
+	setTimeout(function(){
+		_this.timer = setInterval(function(){
+			//_this.width == height
+			if(_this.img.offsetTop > screenHeight - _this.width){
+				clearInterval(_this.timer);
+			}else{
+				_this.img.style.top =_this.img.offsetTop +_this.speed+"px";
+			}
+		},30)
+	},Math.random()*1000);	
+}
+var aLeaf = [];
 for(var i=0; i<7;i++){
 	var oLeaf = new Leaf();
 	oLeaf.init();
+	aLeaf.push(oLeaf);
 }
+document.onclick = function(){
+	for(var i=0; i<aLeaf.length; i++){
+		aLeaf[i].fall();
+	}
+}
+
+
 
 
 
