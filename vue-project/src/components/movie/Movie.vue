0a5127c20328e0a27694e2d34aa19ae0a783cb1b
wuying <wuy_123@126.com>
2017-06-18 16:00:12 +0800

    vue-cli

---
+++ vue-project/src/components/movie/Movie.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="movie">
+    <common-header></common-header>
+    <movie-nav></movie-nav>
+  </div>
+</template>
+
+<script>
+
+  import CommonHeader from '../common/CommonHeader'
+  import MovieNav from './MovieNav'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components:{
+    CommonHeader,
+    MovieNav
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  @import "../../assets/css/reset.css";
+
+
+</style>
---
+++ vue-project/src/components/movie/MovieNav.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li>top250</li>
+      <li>正在热映</li>
+      <li>即将上映</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+    }
+  }
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  @import "../../assets/css/reset.css";
+  .nav{
+    height: 1rem;
+    color: #fff;
+    background: rgb(33, 150, 243);
+  }
+  .nav ul{
+    display: flex;
+  }
+  .nav ul li{
+    flex: 1;
+    height: 1rem;
+    text-align: center;
+    line-height: 1rem;
+  }
+
+</style>
