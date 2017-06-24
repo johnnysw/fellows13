9fbdae56703a21f8b394b4cf92545ce092dd939b
wuying <wuy_123@126.com>
2017-06-24 10:19:28 +0800

    vue

--- vue-project/src/vuex/store.js
+++ vue-project/src/vuex/store.js
@@ -10,19 +10,25 @@ Vue.use(Vuex)
 
 //定义初始化变量
 const state = {
-  title:'movie'
+  title:'movie',
+  bgColor:'rgb(33, 150, 243)'
 }
 
 //定义动作 事件处理方法
 const mutations = {
   changeTitle(state,status){
     state.title = status;
+  },
+  changeBgColor(state,status){
+    state.bgColor = status;
   }
 }
 
 //对外的事件方法
 const actions = {
-  changeTitle:({commit},status)=>commit('changeTitle',status)
+  changeTitle:({commit},status)=>commit('changeTitle',status),
+  changeBgColor:({commit},status)=>commit('changeBgColor',status)
+
 }
 
 const getters = {
@@ -35,4 +41,3 @@ export default new Vuex.Store({
   actions,
   getters
 })
-
