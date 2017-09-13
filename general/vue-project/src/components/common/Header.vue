674902d2a0f78ffbc841a5b0bbf3cc3a44f7def9
wuying <wuy_123@126.com>
2017-09-13 14:01:15 +0800

    vuejs

---
+++ general/vue-project/src/components/common/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="header">
+    <button>首页</button>
+    <h2>
+      movie
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../../assets/css/reset.css';
+  #header{
+    height: 1rem;
+    display: flex;
+    background: rgb(33, 150, 243);
+  }
+  #header button{
+    width: 1rem;
+    height: 1rem;
+    color: #fff;
+  }
+  #header h2{
+    flex:1;
+    height: 1rem;
+    color: #fff;
+    text-align: center;
+    margin-left: -1rem;
+  }
+</style>
