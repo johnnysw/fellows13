7e4421ef1506e6e0d1d85cdcce0c181f26ef880a
wuying <wuy_123@126.com>
2017-09-14 14:28:10 +0800

    vuejs

--- general/vue-project/src/components/music/MusicAlbums.vue
+++ general/vue-project/src/components/music/MusicAlbums.vue
@@ -1,23 +1,28 @@
 <template>
-  <div id="music">
-    <common-header></common-header>
-    <router-view></router-view>
-    <common-footer></common-footer>
-  </div>
+      <ul id="music_albums">
+          <li v-for="alb in list">
+            <img :src="alb.bg">
+          </li>
+      </ul>
 </template>
 
 <script>
   import CommonHeader from '@/components/common/Header'
   import CommonFooter from '@/components/common/Footer'
+  import Axios from 'axios'
 export default {
   name: 'header',
   data () {
     return {
-
+      list:[]
     }
   },
   mounted(){
     this.$store.dispatch('changeTitle',['music','rgb(0, 150, 136)']);
+    Axios.get('/static/musiclist.json').then((res)=>{
+      this.list = res.data.albums;
+    });
+
   },
   components:{
     CommonHeader,
@@ -30,5 +35,20 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   /*@import '../../assets/css/reset.css';*/
+#music_albums{
+  margin-top: 1rem;
+}
+  #music_albums li{
+    float: left;
+    width: 50%;
+    height: 33.33%;
+  }
+
+  #music_albums li img{
+    width: 100%;
+    height: 100%;
+  }
+
+
 
 </style>
