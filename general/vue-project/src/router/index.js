6561a0380aef398b3c95afeb70894e007f364814
wuying <wuy_123@126.com>
2017-09-14 14:47:46 +0800

    vuejs

--- general/vue-project/src/router/index.js
+++ general/vue-project/src/router/index.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Movie from '@/components/movie/Movie'
 import MovieTop250 from '@/components/movie/MovieTop250'
-
+import Music from '@/components/music/Music'
+import Book from '@/components/book/Book'
+import Photo from '@/components/photo/Photo'
+import MusicAlbums from '@/components/music/MusicAlbums'
 Vue.use(Router)
 
 export default new Router({
@@ -16,8 +19,33 @@ export default new Router({
       name: 'Movie',
       component: Movie,
       children: [
-        { path: "/movie/top250", component: MovieTop250 }
+        { path: "/movie/top250", component: MovieTop250 },
+        { path: "/movie/hot", component: MovieTop250 },
+        { path: "/movie/coming", component: MovieTop250 }
       ]
+    },
+    {
+      path: '/music',
+      redirect:'/music/music_albums'
+    },
+    {
+      path: '/music',
+      name: 'Music',
+      component: Music,
+      children: [
+        { path: "/music/music_albums", component: MusicAlbums },
+        { path: "/music/music_list", component: MusicAlbums },
+      ]
+    },
+    {
+      path: '/book',
+      name: 'Book',
+      component: Book
+    },
+    {
+      path: '/photo',
+      name: 'Photo',
+      component: Photo
     }
   ]
 })
